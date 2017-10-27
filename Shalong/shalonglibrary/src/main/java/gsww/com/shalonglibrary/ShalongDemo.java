0e70b79cd434fd91fa79137261bd88a3076f5a96
liufx <547166147@qq.com>
2017-10-27 16:07:33 +0800

    初始提交。。。

---
+++ Shalong/shalonglibrary/src/main/java/gsww/com/shalonglibrary/ShalongDemo.java
@@ -0,0 +1,11 @@
+package gsww.com.shalonglibrary;
+
+/**
+ * Created by liufx on 2017/10/27.
+ */
+
+public class ShalongDemo {
+    public static String getStr(){
+        return "323323";
+    }
+}
